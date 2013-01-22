9f20e89937305096eb69737ad60dcd28ef4c9d60
Anathema <professionalnihilist@gmail.com>
2013-01-22 20:11:54 +0000

    add thread capture to scraper, refactor xpath

--- Nishkriya/Scraper/ScavengerLord.cs
+++ Nishkriya/Scraper/ScavengerLord.cs
@@ -25,25 +25,25 @@ namespace Nishkriya.Scraper
         {
             using (var db = new NishkriyaContext())
             {
-                db.Accounts.ToList().ForEach(account => account.Posts.AddRange(GetNewPosts(account)));
+                db.Accounts.ToList().ForEach(account => account.Posts.AddRange(GetNewPosts(account, db.Threads.ToList())));
                 db.SaveChanges();
             }
         }
 
-        private IEnumerable<Post> GetNewPosts(ForumAccount account)
+        private IEnumerable<Post> GetNewPosts(ForumAccount account, List<Thread> threads)
         {
             try
             {
                 var url = string.Format(Settings.Default.ProfileUrl, account.ForumId);
 
-                var req = (HttpWebRequest) WebRequest.Create(url);
+                var req = (HttpWebRequest)WebRequest.Create(url);
                 req.Method = "GET";
                 req.ContentType = "application/x-www-form-urlencoded";
                 req.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
                 req.CookieContainer = new CookieContainer();
                 req.CookieContainer.Add(new Cookie(".YAFNET_Authentication", Settings.Default.AuthToken, "/", "forums.white-wolf.com"));
 
-                var response = (HttpWebResponse) req.GetResponse();
+                var response = (HttpWebResponse)req.GetResponse();
                 var document = new HtmlDocument();
 
                 using (var reader = new StreamReader(response.GetResponseStream()))
@@ -61,37 +61,45 @@ namespace Nishkriya.Scraper
 
                 var postsCollection = new List<Post>();
 
-                foreach (int i in Enumerable.Range(1, 10))
+                const string placeholderFragment = "id('MasterPageContentPlaceHolder_forum_ctl01_ProfileTabs_Last10PostsTab')//table//tr[";
+                const string anchorSelectorFragment = "]//td/a/@href";
+                const string titleSelectorFragment = "]//td/a/text()";
+                const string dateSelectorFragment = "]//td/text()[4]";
+
+                foreach (int i in Enumerable.Range(0, 10))
                 {
                     var tableRow = 1 + (2 * i);
 
-                    var threadHref =
-                    document.DocumentNode.SelectSingleNode(
-                        "id('MasterPageContentPlaceHolder_forum_ctl01_ProfileTabs_Last10PostsTab')//table//tr[" + tableRow + "]//td/a/@href").Attributes[0].Value;
+                    var threadIdSelector = String.Format("{0}{1}{2}", placeholderFragment, tableRow, anchorSelectorFragment);
+                    var titleSelector = String.Format("{0}{1}{2}", placeholderFragment, tableRow, titleSelectorFragment);
+                    var dateSelector = String.Format("{0}{1}{2}", placeholderFragment, tableRow, dateSelectorFragment);
+
+                    var threadHref = document.DocumentNode.SelectSingleNode(threadIdSelector).Attributes[0].Value;
                     var threadId = int.Parse(Regex.Match(threadHref, @"(\d+)$").Groups[0].Value);
 
-                    var threadTitle =
-                        document.DocumentNode.SelectSingleNode(
-                            "id('MasterPageContentPlaceHolder_forum_ctl01_ProfileTabs_Last10PostsTab')//table//tr[" + tableRow + "]//td/a/text()").InnerHtml.Trim();
+                    var threadTitle = document.DocumentNode.SelectSingleNode(titleSelector).InnerHtml.Trim();
 
-                    var postDate = DateTime.Parse(
-                        document.DocumentNode.SelectSingleNode(
-                            "id('MasterPageContentPlaceHolder_forum_ctl01_ProfileTabs_Last10PostsTab')//table//tr[" + tableRow + "]//td/text()[4]").InnerHtml.Trim());
+                    var postDate = DateTime.Parse(document.DocumentNode.SelectSingleNode(dateSelector).InnerHtml.Trim());
 
                     var postContent =
                         document.DocumentNode.SelectSingleNode(
                             "id('MasterPageContentPlaceHolder_forum_ctl01_ProfileTabs_Last10PostsTab_LastPosts_MessagePost_" + i + "')").InnerHtml;
 
+                    Thread thread = threads.SingleOrDefault(s => s.ThreadId == threadId);
+                    if (thread == null)
+                    {
+                        thread = new Thread { ThreadId = threadId, Title = threadTitle };
+                        threads.Add(thread);
+                    }
+
                     postsCollection.Add(new Post
                     {
                         Content = postContent,
                         Hash = _hashProvider.Compute(postContent),
-                        ThreadId = threadId,
                         PostDate = postDate,
-                        PostTitle = threadTitle,
+                        Thread = thread
                     });
                 }
-
 
 
                 return postsCollection.Where(newPost => !account.Posts.Select(p => p.Hash).Contains(newPost.Hash));
@@ -100,7 +108,7 @@ namespace Nishkriya.Scraper
             {
                 using (var db = new NishkriyaContext())
                 {
-                    db.Errors.Add(new Error {StackTrace = ex.StackTrace, OccurredOn = DateTime.Now});
+                    db.Errors.Add(new Error { StackTrace = ex.StackTrace, OccurredOn = DateTime.Now });
                     Debug.WriteLine(ex.Message + "\n----\n" + ex.StackTrace);
                 }
                 return new Post[0];
