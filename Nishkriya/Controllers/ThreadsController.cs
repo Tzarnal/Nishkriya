884c8739fe644c214673b459a886feb62e240f46
Xesyto <M8R-acpfdp@mailinator.com>
2013-01-23 17:42:42 +0100

    Created new Threads Controller
    Added Details Index And LatestTopics view stubs for the Threads
    Controller
    Added basic functionality to Details View of Threads Controller

---
+++ Nishkriya/Controllers/ThreadsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Nishkriya.Models;
+
+namespace Nishkriya.Controllers
+{
+    public class ThreadsController : Controller
+    {
+        NishkriyaContext db = new NishkriyaContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LatestTopics()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            var thread = db.Threads.Single(t => t.ThreadId == id);
+
+            ViewBag.Title = thread.Title;
+            return View(thread);
+        }
+    }
+}
