1e8cb47c61415978028b3da50a10e30d57b55593
Xesyto <M8R-acpfdp@mailinator.com>
2013-01-29 22:09:08 +0100

    Ternaries operate the other way around

--- Nishkriya/Models/Post.cs
+++ Nishkriya/Models/Post.cs
@@ -38,14 +38,14 @@ namespace Nishkriya.Models
             string result;
             if (timeDifference.TotalHours < 1)
             {
-                result = Math.Round(timeDifference.TotalMinutes) == 1 ? string.Format("{0} Minutes ago", Math.Round(timeDifference.TotalMinutes)) : "1 Minute Ago";                
+                result = Math.Round(timeDifference.TotalMinutes) == 1 ? "1 Minute Ago" : string.Format("{0} Minutes ago", Math.Round(timeDifference.TotalMinutes));                
             }else if (timeDifference.TotalDays < 1)
             {
-                result = Math.Round(timeDifference.TotalHours) == 1 ? string.Format("{0} Hours ago", Math.Round(timeDifference.TotalHours)) : "1 Hour Ago";  
+                result = Math.Round(timeDifference.TotalHours) == 1 ? "1 Hour Ago" : string.Format("{0} Hours ago", Math.Round(timeDifference.TotalHours));  
             }
             else if (timeDifference.TotalDays < 7)
             {
-                result = Math.Round(timeDifference.TotalDays) == 1 ? string.Format("{0} Days ago", Math.Round(timeDifference.TotalDays)) : "1 Day Ago";  
+                result = Math.Round(timeDifference.TotalDays) == 1 ?  "1 Day Ago" : string.Format("{0} Days ago", Math.Round(timeDifference.TotalDays));  
             }
             else
             {
