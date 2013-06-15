44afb570a98e3062a60f254c034b91c16ad51d10
Anathema <professionalnihilist@gmail.com>
2013-06-15 22:03:42 +0100

    Optimisation and publishing
    Edit the CSS and JS bundles to have extensions and tell IIS to let us
    handle those paths. This lets us output the bundles to those requests and
    because it has an extension IIS will be smart enough to compress it for
    us.
    
    Update publishing profile to generate an installable zip instead of just
    dumping the files where the old site was hosted.

--- Nishkriya/App_Start/BundleConfig.cs
+++ Nishkriya/App_Start/BundleConfig.cs
@@ -7,13 +7,13 @@ namespace Nishkriya
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/scripts")
+            bundles.Add(new ScriptBundle("~/bundles/scripts.js")
                             .Include("~/Scripts/jquery-{version}.js")
                             .Include("~/Scripts/bootstrap.js")
                             .Include("~/Scripts/spoiler.js")
                             .Include("~/Scripts/nishkriya.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/css")
+            bundles.Add(new StyleBundle("~/bundles/styles.css")
                             .Include("~/Content/css/bootstrap.css")
                             .Include("~/Content/css/bootstrap-responsive.css")
                             .Include("~/Content/site.css"));
