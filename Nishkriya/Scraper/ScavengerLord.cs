2b46dd92164ba9d7ad993d3555f05d4260ec6068
Anathema <me@theanathema.com>
2013-11-07 08:13:34 +0000

    Get accounts in each scraper, not in the manager.

--- Nishkriya/Scraper/ScavengerLord.cs
+++ Nishkriya/Scraper/ScavengerLord.cs
@@ -12,7 +12,6 @@ namespace Nishkriya.Scraper
         {
             using (var db = new NishkriyaContext())
             {
-                var accounts = db.Accounts.Where(a => a.Active).ToList();
                 var hasher = new Sha1Provider();
                 var scrapers = new List<IForumScraper> {new YAFScavenger(hasher, db), new VBulletinScavenger(db)};
                 scrapers.ForEach(s => s.Scrape());
