f18143d2c12a6cce283b8ab3122195228de381b4
Xesyto <M8R-acpfdp@mailinator.com>
2013-11-08 18:09:07 +0100

    Fixed urls to specific posts in Vbulliten posts Changed all instances of displaying posts to the _post partial now that we can produce proper links to specific posts on the forums.

--- Nishkriya/Models/Post.cs
+++ Nishkriya/Models/Post.cs
@@ -48,7 +48,7 @@ namespace Nishkriya.Models
                 Content = this.Content,
                 PostDate = this.PostDate,
                 Thread = thread,
-                Url = this.PostId.HasValue ? new Uri(thread.Url + "/"+  PostId.Value) : thread.Url,
+                Url = this.PostId.HasValue ? new Uri(thread.Url + "#post"+  PostId.Value) : thread.Url,
             };
         }
     }
